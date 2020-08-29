ae858e04f978223ffc418f3c50dd77226f672463
Senorui <ghr239@163.com>
2020-08-29 23:22:32 +0800

    Site updated: 2020-08-29 23:22:31

--- music/js/index.js
+++ music/js/index.js
@@ -43,7 +43,43 @@ $(function () {
         }, 300);
     }
 
-
+function curl_http($url, $isPostRequest=false, $data=[], $header=[], $certParam=[]){ // 模拟提交数据函数
+    $curlObj = curl_init(); // 启动一个CURL会话
+    //如果是POST请求
+    if( $isPostRequest ){
+        curl_setopt($curlObj, CURLOPT_POST, 1); // 发送一个常规的Post请求
+        curl_setopt($curlObj, CURLOPT_POSTFIELDS, http_build_query($data)); // Post提交的数据包
+    }else{  //get请求检查是否拼接了参数，如果没有，检查$data是否有参数，有参数就进行拼接操作
+        $getParamStr = '';
+        if(!empty($data) && is_array($data)){
+            $tmpArr = [];
+            foreach ($data as $k=>$v){
+                $tmpArr[] = $k . '=' . $v;
+            }
+            $getParamStr = implode('&', $tmpArr);
+        }
+        //检查链接中是否有参数
+        $url .= strpos($url, '?') !== false ? '&' . $getParamStr : '?' . $getParamStr;
+    }
+    curl_setopt($curlObj, CURLOPT_URL, $url); // 要访问的地址
+    //检查链接是否https请求
+    if(strpos($url, 'https') !== false){
+        //设置证书
+        if( !empty($certParam) && isset($certParam['cert_path']) && isset($certParam['key_path']) ){
+            curl_setopt($curlObj, CURLOPT_SSL_VERIFYPEER, 0); // 对认证证书来源的检查
+            curl_setopt($curlObj, CURLOPT_SSL_VERIFYHOST, 2); // 从证书中检查SSL加密算法是否存在
+            //设置证书
+            //使用证书：cert 与 key 分别属于两个.pem文件
+            curl_setopt($curlObj, CURLOPT_SSLCERTTYPE,'PEM');
+            curl_setopt($curlObj, CURLOPT_SSLCERT, $certParam['cert_path']);
+            curl_setopt($curlObj, CURLOPT_SSLKEYTYPE,'PEM');
+            curl_setopt($curlObj, CURLOPT_SSLKEY, $certParam['key_path']);
+        }else{
+            curl_setopt($curlObj, CURLOPT_SSL_VERIFYPEER, 0); // 对认证证书来源的检查
+            curl_setopt($curlObj, CURLOPT_SSL_VERIFYHOST, 0); // 从证书中检查SSL加密算法是否存在
+        }
+    }
+    
     function showHover(event) {
         seekBarPos = sArea.offset();
         seekT = event.clientX - seekBarPos.left;
@@ -211,7 +247,7 @@ $(function () {
                 $('#album-pic').attr('src',currArtwork);
 
                 bgArtwork.css({
-                    'background-image': 'url(https:' + currArtwork + ')'
+                    'background-image': 'url(' + currArtwork + ')'
                 });
             }
         });
